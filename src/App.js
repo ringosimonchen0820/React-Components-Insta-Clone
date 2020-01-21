222aa8b5d6ed66f5e3d33dc65dfd1e35f50aa527
SimonChen <ringosimonchen0820@gmail.com>
2020-01-21 16:24:25 -0500

    setup app layout in App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,26 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
+
 // import the PostsPage and SearchBar and add them to the App
+import SearchBar from "./components/SearchBar/SearchBarContainer";
+import PostsPage from "./components/PostsContainer/PostsPage";
 // import dummydata
+import dummyData from "./dummy-data";
 
 const App = () => {
   // set up state for the dummy data and pass to your PostsPage
+  const [data] = useState(dummyData);
+  console.log("this is the dummyData:", data);
   return (
     <div className="App">
       {/* Add components here  and pass props where appropriate */}
+      <SearchBar/>
+      <PostsPage
+        postData = {data}
+      />
     </div>
   );
 };
